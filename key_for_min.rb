37dbb05d6f7da7e91c93ef4cece02c6f82f5b850
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 13:29:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = 0
   min_key = nil
   name_hash.each do |key, value|
-    if min_value > value || min_value == 0
+    if min_value == 0
       min_value = value
       min_key = key
     end
